f82788a47fbc3d8cff844be3bf290f821a2c438f
Vitória Reimão <vi.reimao@gmail.com.br>
2022-07-05 00:19:02 -0300

    ajustando a suite home para os padrões do cypress.json

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,7 +1,6 @@
 describe('home page', ()=>{ //Definindo a Suite de Teste
     it('app deve estar online', ()=>{ //Criando o Caso de Teste
-        cy.viewport(1440, 900)
-        cy.visit('https://buger-eats.vercel.app')
+        cy.visit('/')
         cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
 
     })
